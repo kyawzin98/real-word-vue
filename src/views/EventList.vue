f0481de7deb9e562fa8cc164123f340bfe996979
kyawzin <kyawzinmcci00158313@gmail.com>
2020-08-16 10:22:57 +0630

    Lessen 5,6,7,8 - finished real world vue

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Event List</div>
+  <div>
+    <h2>Event List</h2>
+    <event-card v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
 
 <script>
+import EventCard from "../components/EventCard";
+import EventServices from "../services/EventServices";
 export default {
-  name: "EventList"
+  name: "EventList",
+  components: { EventCard },
+  props: ["id"],
+  data: () => ({
+    events: []
+  }),
+  created() {
+    EventServices.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(e => {
+        console.log("There was a error" + e.response);
+      });
+  }
 };
 </script>
 
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,11 +1,75 @@
 <template>
-  <div>Event Show</div>
+  <div>
+    <div class="event-header">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : "" }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+
+    <address>{{ event.location }}</address>
+
+    <h2>Event details</h2>
+    <p>{{ event.description }}</p>
+
+    <h2>
+      Attendees
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
+    </h2>
+    <ul class="list-group">
+      <li
+        v-for="(attendee, index) in event.attendees"
+        :key="index"
+        class="list-item"
+      >
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import EventServices from "../services/EventServices";
+
 export default {
-  name: "EventShow"
+  name: "EventShow",
+  props: ["id"],
+  data: () => ({
+    event: {}
+  }),
+  created() {
+    EventServices.getEvent(this.id)
+      .then(res => {
+        this.event = res.data;
+      })
+      .catch(e => {
+        console.log("There was a error " + e.response);
+      });
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
